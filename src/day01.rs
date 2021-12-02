72431d128d1b6077c196a2a9aaece1a56ef27f04
Lucas Joyal <lucasjoyal@gmail.com>
2021-12-01 20:05:56 -0500

    Complete part 1 of day01
    Rust is nice to write. Doing the shorelark tutorial really helped
    expanding my knowledge of the standard library.

--- src/day01.rs
+++ src/day01.rs
@@ -2,11 +2,28 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn day_01() -> std::io::Result<()> {
-    let mut file = File::open("./resources/day01_small.txt")?;
+    // let mut file = File::open("./resources/day01_small.txt")?;
+    let mut file = File::open("./resources/day01.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
     println!("contents:\n{}", contents);
+
+    let report: Vec<i32> = contents
+        .trim()
+        .split('\n')
+        .map(|line| line.trim().parse::<i32>().unwrap())
+        .collect();
+
+    let increase_count = report
+        .windows(2)
+        .filter(|entries| entries[0] < entries[1])
+        .count();
+
+    println!(
+        "There are {} measurements that are larger than the previous one",
+        increase_count,
+    );
 
     return Ok(());
 }
