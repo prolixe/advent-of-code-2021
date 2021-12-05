29b5d5c68b9608ce17d7ff04d8a07b629f900b80
Lucas Joyal <lucasjoyal@gmail.com>
2021-12-05 09:38:43 -0500

    Complete part 1 and 2 of day 04

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     //day01::day_01().unwrap();
     //day02::day_02().unwrap();
-    day03::day_03().unwrap();
+    day04::day_04().unwrap();
 }
